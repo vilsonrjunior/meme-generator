ae07f0df839cf953e257ac1e4e633468d04aa58c
vilson junior <vilsonrjunior@gmail.com>
2019-08-22 11:39:18 +0100

    added api call and saved result to state

--- src/Components/MemeGenerator.js
+++ src/Components/MemeGenerator.js
@@ -3,15 +3,60 @@ import React, { Component } from "react"
 class MemeGenerator extends Component {
   constructor() {
     super()
-    this.state={}
+    this.state={
+      topText: "",
+      bottomText: "",
+      randomImage: "",
+      allMemeImgs: []
+    }
+    this.handleChange = this.handleChange.bind(this)
   }
+
+componentDidMount() {
+  fetch("https://api.imgflip.com/get_memes")
+    .then(response => response.json())
+    .then(response => {
+      const {memes} = response.data
+      this.setState({ allMemeImgs: memes })
+    })
+}
+
+handleChange(event) {
+  const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+}
+
 
 
 render() {
+
+  console.log(this.state.topText)
+  console.log(this.state.bottomText)
+
    return(
-    <div>
-      <h1>From MemeGenerator</h1>
-    </div>
+      <div>
+          <form className="meme-form">
+          <input
+              type="text"
+              value={this.state.topText}
+              name="topText"
+              placeholder="Top text"
+              onChange={this.handleChange}
+              />
+              <br/>
+          <input
+              type="text"
+              className="bottom"
+              value={this.state.bottomText}
+              name="bottomText"
+              placeholder="Bottom text"
+              onChange={this.handleChange}
+              />
+            <button>Gen</button>
+          </form>
+      </div>
     )
   }
 }
